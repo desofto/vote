52cca1aa63b413f0879579aa505861895a87b24d
Dmitry Silchenko <dmitry@desofto.com>
2021-10-12 13:59:30 +0200

    [front] add logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import Login from './Login'
 import User from './User'
 import Admin from './Admin'
+import actions from './actions'
 
 function App() {
   const currentUser = useSelector(store => store.currentUser)
+  const dispatch = useDispatch()
 
   function Dashboard() {
     return (
       <>
+        <button onClick={() => actions.currentUser.logout(dispatch)()}>Logout</button>
         {
           currentUser.is_admin
           ? <Admin />
--- client/src/reducers/current-user.js
+++ client/src/reducers/current-user.js
@@ -1,11 +1,11 @@
 export const UPDATE = 'CURRENT-USER.UPDATE'
 
-const InitialState = {
-}
+const InitialState = JSON.parse(localStorage.getItem('currentUser')) || {}
 
 export default function reducer(state = InitialState, action) {
   switch (action.type) {
     case UPDATE:
+      localStorage.setItem('currentUser', JSON.stringify(action.payload))
       return action.payload
     default:
       return state
