47e2cc9a8301384c21dd577aa1456d190c903aef
Dmitry Silchenko <dmitry@desofto.com>
2021-10-12 14:34:55 +0200

    [front] add tabs on admin

--- client/src/admin/index.js
+++ client/src/admin/index.js
@@ -1,6 +1,46 @@
+import { useState } from 'react'
+import { Tabs, Tab } from 'react-bootstrap'
+
 function Admin() {
+  const [tab, setTab] = useState('events')
+
   return (
-    <div>Admin</div>
+    <Tabs
+      activeKey={tab}
+      onSelect={tab => setTab(tab)}
+      className="mb-3"
+    >
+      <Tab eventKey="events" title="Events">
+        <Events />
+      </Tab>
+      <Tab eventKey="users" title="Users">
+        <Users />
+      </Tab>
+    </Tabs>
+  )
+}
+
+function Events() {
+  return (
+    <div>Events</div>
+  )
+}
+
+function Stages() {
+  return (
+    <div>Stages</div>
+  )
+}
+
+function Teams() {
+  return (
+    <div>Teams</div>
+  )
+}
+
+function Users() {
+  return (
+    <div>Users</div>
   )
 }
 
