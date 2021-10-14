a711584d35069bb089539fbcbbddc90bf7fdd506
Dmitry Silchenko <dmitry@desofto.com>
2021-10-14 19:25:23 +0200

    [front] improve login form

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
+import { Form, Button } from 'react-bootstrap'
 
 import actions from './actions'
 
@@ -13,16 +14,19 @@ function Login() {
   }
 
   return (
-    <div>
-      <div>
-        <label>AccessCode:</label>
-      </div>
-
-      <div>
-        <form onSubmit={login}>
-          <input type="text" value={code} onChange={e => setCode(e.target.value)} />
-          <input type="submit" value="&gt;" />
-        </form>
+    <div className="d-flex text-center" style={{ width: '100vw', height: '100vh' }}>
+      <div className="m-auto px-5 py-4" style={{ border: '1px solid #eee', borderRadius: '2em' }}>
+        <Form onSubmit={login}>
+          <Form.Group className="mb-3">
+            <Form.Label className="fw-bolder">Access Code:</Form.Label>
+            <div className="d-flex">
+              <Form.Control type="text" value={code} onChange={e => setCode(e.target.value)} />
+              <Button variant="primary" onClick={login}>
+                &gt;
+              </Button>
+            </div>
+          </Form.Group>
+        </Form>
       </div>
     </div>
   )
