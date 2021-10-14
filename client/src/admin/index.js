9b81d8c1f141dcef0f5625dccf3bcbffcaa04b29
Dmitry Silchenko <dmitry@desofto.com>
2021-10-14 12:24:46 +0200

    [front] create events in modal, show errors during creation

--- client/src/admin/index.js
+++ client/src/admin/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Tabs, Tab, Table } from 'react-bootstrap'
+import { Tabs, Tab, Table, Button, Modal, Form } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import actions from '../actions'
 
@@ -59,24 +59,63 @@ function Admin() {
 }
 
 function Events({ onSelect }) {
-  const EMPTY_EVENT = { title: '', date: '' }
   const events = useSelector(store => store.events)
-  const [event, setEvent] = useState(EMPTY_EVENT)
   const dispatch = useDispatch()
 
   useEffect(async function() {
     await actions.events.load(dispatch)()
   }, [dispatch])
 
-  async function create(e) {
-    e.preventDefault()
-    await actions.events.add(dispatch)(event)
-    setEvent(EMPTY_EVENT)
-  }
-
   function openEvent(e, id) {
     e.preventDefault()
     onSelect(id)
+  }
+
+  function NewEvent() {
+    const [show, setShow] = useState(false)
+    const EMPTY_EVENT = { title: '', date: '' }
+    const [event, setEvent] = useState(EMPTY_EVENT)
+
+    async function create(e) {
+      e.preventDefault()
+      if (!await actions.events.add(dispatch)(event)) return
+      setEvent(EMPTY_EVENT)
+    }
+
+    return (
+      <>
+        <Button variant="primary" className="fa" onClick={() => setShow(true)}>
+          &#xf067;
+        </Button>
+
+        <Modal show={show} onHide={() => setShow(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Create a new Event</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Label>Event title</Form.Label>
+                <Form.Control type="text" value={event.title} onChange={e => setEvent({ ...event, title: e.target.value })} />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Event starts</Form.Label>
+                <Form.Control type="date" value={event.date} onChange={e => setEvent({ ...event, date: e.target.value })} />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={() => setShow(false)}>
+              Cancel
+            </Button>
+            <Button variant="primary" onClick={create}>
+              Create
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    )
   }
 
   return (
@@ -84,10 +123,7 @@ function Events({ onSelect }) {
       <div className="d-flex">
         <div className="flex-grow-1">Events</div>
         <div>
-          <label className="me-1">New event:</label>
-          <input className="me-1" type="text" value={event.title} onChange={e => setEvent({ ...event, title: e.target.value })} />
-          <input className="me-1" type="date" value={event.date} onChange={e => setEvent({ ...event, date: e.target.value })} />
-          <input type="submit" value="&#xf067;" className="fa" onClick={create} />
+          <NewEvent />
         </div>
       </div>
 
